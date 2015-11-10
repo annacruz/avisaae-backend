12d49f91e1d51c0c7539594c72373076957552f6
Anna Cruz <anna.cruz@gmail.com>
2015-11-10 16:46:36 -0200

    Starting the server development

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "flag"
+)
+
+var (
+  port *int
+)
+
+func init(){
+  port = flag.Int("p", 8888, "port")
+  flag.Parse()
+}
+
+func ReturnData(){
+  
+}
+
+func main() {
+  http.HandleFunc('/api/data', ReturnData)
+
+
+  logar("Iniciando servidor na porta %d...", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
