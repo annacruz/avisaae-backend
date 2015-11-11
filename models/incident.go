95f2f2ca68e51d45c28a821eaf0b4a88299d49a2
Anna Cruz <anna.cruz@gmail.com>
2015-11-11 11:31:40 -0200

    Adding incident model

---
+++ models/incident.go
@@ -0,0 +1,17 @@
+package main
+
+import(
+  "time"
+)
+
+type Incident struct {
+  Id int
+  Details string
+  Type string
+  DateTimeIncident time.Time
+  CreationDateTime time.Time
+}
+
+func main() {
+  
+}
